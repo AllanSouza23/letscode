37f657f614e205a3e16abbb5fbecb587a0a77d9b
Allan Souza <allanronaldo95@gmail.com>
2021-07-08 14:58:15 -0300

    Criando um componente simples

--- reactComponentesTipoClasse/meu-app/src/App.js
+++ reactComponentesTipoClasse/meu-app/src/App.js
@@ -1,9 +1,22 @@
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      // Quando há mais de um elemento a ser renderizado, devemos envolvê-lo por uma div, ou até mesmo com um fragmento, ou seja, uma tag "fantasma", conforme abaixo:
+      <>
+        <p>
+          Meu primeiro parágrafo
+        </p>
+        <p>
+          Meu segundo parágrafo
+        </p>
+        {/* Para usar o JavaScript dentro do nosso componente em React devemos colocá-lo entre chaves. */}
+        <p>
+          {new Date().toLocaleDateString('pt-br')}
+        </p>
+      </>
+    );
+  }
 }
-
 export default App;
